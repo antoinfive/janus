c14f15e33c753b8bc9a186c8c72fadeac0e676dc
sareid <andrew.reid@flatironschool.com>
2016-02-17 17:37:56 -0500

    Fixes routes for namespacing

--- janus-api/config/routes.rb
+++ janus-api/config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  resources :snippets, except: [:new, :edit]
-  resources :tags, except: [:new, :edit]
-  resources :bookmarks, except: [:new, :edit]
+
   namespace :api do
     namespace :v1 do
       resources :stories, except: [:new, :edit]
       resources :projects, except: [:new, :edit]
       resources :users, except: [:new, :edit]
+      resources :snippets, except: [:new, :edit]
+      resources :tags, except: [:new, :edit]
+      resources :bookmarks, except: [:new, :edit]
     end
   end
+
 end

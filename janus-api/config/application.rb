7e9cdb768429a7c77f418481ec118e68058e62a0
Aemrox <adam.may2164@gmail.com>
2016-02-18 11:32:16 -0500

    Added Sophie's Cors code, no idea if it works

--- janus-api/config/application.rb
+++ janus-api/config/application.rb
@@ -14,5 +14,12 @@ module JanusApi
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.api_only = true
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+      end
+    end
   end
 end

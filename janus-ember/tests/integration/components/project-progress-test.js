443a7e380dcebc95f30d6a0e7b2ba04413b766ea
Aemrox <adam.may2164@gmail.com>
2016-02-21 16:53:44 -0500

    Dashboard works, dashboard has a project show page that needs to be built out but currently has a header with basic project information

--- janus-ember/tests/integration/components/project-progress-test.js
+++ janus-ember/tests/integration/components/project-progress-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('dashboard/project-preview', 'Integration | Component | dashboard/project preview', {
+moduleForComponent('project-progress', 'Integration | Component | project progress', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
 
-  this.render(hbs`{{dashboard/project-preview}}`);
+  this.render(hbs`{{project-progress}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:" + EOL +
   this.render(hbs`
-    {{#dashboard/project-preview}}
+    {{#project-progress}}
       template block text
-    {{/dashboard/project-preview}}
+    {{/project-progress}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');

6bdba69e1c89b904fd4a93c65ee44b7834971acf
Aemrox <adam.may2164@gmail.com>
2016-02-22 17:35:21 -0500

    nothing really

--- janus-ember/app/serializers/bookmark.js
+++ janus-ember/app/serializers/bookmark.js
@@ -2,7 +2,8 @@ import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend(DS.EmbeddedRecordsMixin, {
   attrs:{
-    projects: {serialize: 'ids'}
+    projects: {serialize: 'ids'},
+    tags: {serialize: 'ids'}
   },
   isNewSerializerAPI: true
 });
